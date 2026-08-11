1a74648fccc76eb0a556f85a29f1313be3fcd8b3
aryabc2004 <aryabc04@gmail.com>
2026-08-10 17:47:19 -0700

    Added more categories

--- src/categories.jsx
+++ src/categories.jsx
@@ -6,9 +6,10 @@ const categories = {
   "Basketball Players": ["Michael Jordan","Lebron James","Wilt","Bill Russell","Kareem","Steph Curry","Kevin Durant","Kevin Garnett","Magic Johnson","Larry Bird","Allen Iversen","Carmelo Anthony","Chris Paul","Wembanyama","Jokic","Luka Doncic","Elton Brand","Chris Webber","Dirk","Cam Thomas","Michael Porter James","Jamal Murray","Giannis"],
   "Fictional Characters":["Indiana Jones","Spider-Man","Eren Yeager","James Bond","Dracula","Sherlock Holmes","Homer Simpson","Hercules","Maleficent","Harry Potter","Anakin Skywalker","Wonder Woman","Superman","Batman","Wolverine","Ironman"],
   "Cities":["New York","Moscow","Delhi","London","Paris","Tokyo","Rome","Athens","Rio de Janeiro","Cairo","Sydney","Hong Kong","Chicago","Los Angeles","San Francisco","Istanbul"],
-  "Countries":["","","","4","","","","8","","","","12","","","",""],
-  "TV Shows":["","","","4","","","","8","","","","12","","","",""],
-  "Historical Figures":["","","","4","","","","8","","","","12","","","",""],
+  "Countries":["UK","Spain","Japan","Brazil","France","USA","Italy","Australaia","Germany","Mexico","India","Thailand","Canada","China","Egypt","Iran"],
+  "States":["California","Texas","Alabama","Hawaii","Florida","Montana","Nevada","Mississsipi","North Carolina","New York","Kentucky","Tennessee","Colorodo","Washington","Illinois","Alaska"],
+  "TV Shows":["Friends","Sex and the City","Star Trek","The Walking Dead","Breaking Bad","SpongeBObSquarePants","Cheers","SUccession","Game of Thrones","Sopranos","Greys Anatomy","Young Sheldon","SNL","South Park","How I Met Your Mother","Invincible"],
+  "Historical Figures":["Jesus","Napoleon","Stalin","Hitler","Darwin","MLK","Dr. MLK","Einstein","Columbus","Rosa Parks","Anne Frank","Julius Caesar","Moazart","Cleopatra","Buddha","Mohammed","Churchill","FDR","Mussolini","Hirohito","Oppenhiemer"],
   "Mythical Creatures":["Cyclops","Peagsus","Medusa","Bigfoot","Werewolf","Unicorn","Dragon","Troll","Loch Ness Monster","Mermaid","Phoenix","Vampire","Minotaur","Hydra","Yeti","Centaur"],
   "World Wonders":["Pyramids","Eiffel Tower","Statue of Liberty","Big Ben","Stonehenge","Golden Gate Bridge","Colosseum","Sydney Opera House","Christ The Redeemer","Machu Picchu","Taj Mahal","Hoover Dam","Great Wall of China","Mount Rushmore","Empire State Building","Leaning Tower of Pisa"],
 }
